83bdededfe4758248e7367ec9f13a5867dfe6c72
Owen Johnson <owen@owenjohnson.info>
2011-07-21 15:10:15 -0700

    Edited update.php via GitHub

--- update.php
+++ update.php
@@ -6,6 +6,7 @@
 <div class="bodywrap">
 <h1>Upgrading Date2Knight!</h1>
 <div>
+<?php passthru("git init"); ?>
 <?php passthru("git pull git://github.com/WartburgComputerClub/date2knight.git"); ?>
 </div>
 <h2>Upgrade process finished!</h2>
