dd3f5bc1bcd8cc434ae3e33091fbe2a3b448a270
Andrew Reisner <andrew.reisner@gmail.com>
2011-07-23 11:39:53 -0700

    bug fix on connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,1 @@
-<?php require_once("dbauth.php"); mysql_connect(DbAuth::$host, DbAuth::$user,DbAuth::$password);mysql_query("USE DbAuth::$db");?>
+<?php require_once("dbauth.php"); mysql_connect(DbAuth::$host, DbAuth::$user,DbAuth::$password);$sql="USE " . DbAuth::$db;mysql_query($sql);?>
