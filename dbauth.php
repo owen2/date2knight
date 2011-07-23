b77d248ffc2d7bc378d9902200210b6b6395e87e
Andrew Reisner <andrew.reisner@gmail.com>
2011-07-23 10:48:25 -0700

    struct class for db authentication

---
+++ dbauth.php
@@ -0,0 +1,8 @@
+<?php
+class DbAuth
+{
+    public static $host = 'mysql.owenjohnson.info';
+    public static $user = 'lovematch';
+    public static $password = 'lovematch';
+    public static $db = 'lovematch';
+}
