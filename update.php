d1ec22bded848ad8817473c4ffa2ee3a6c4b7adf
Owen Johnson <owen@owenjohnson.info>
2011-07-24 15:36:59 -0700

    Fixed update.php

--- update.php
+++ update.php
@@ -7,7 +7,7 @@
 <h1>Upgrading Date2Knight!</h1>
 <div>
 <?php passthru("git init"); ?>
-<?php passthru("git pull git://github.com/WartburgComputerClub/date2knight.git"); ?>
+<?php passthru("git pull"); ?>
 </div>
 <h2>Upgrade process finished!</h2>
 </div>
