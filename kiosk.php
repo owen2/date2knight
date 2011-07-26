28ec02a420f86aca3427e06f0948764848bf8808
Andrew Reisner <andrew.reisner@gmail.com>
2011-07-25 20:44:39 -0700

    integrate grid

--- kiosk.php
+++ kiosk.php
@@ -12,6 +12,7 @@ if (!checkSimplePasskey("missingbytes", $_REQUEST['pass']))
       <script type="text/javascript" src="js/jquery-1.5.1.min.js"></script>
       <script type="text/javascript" src="js/jquery-ui-1.8.13.custom.min.js"></script>
       <script type="text/javascript" src="js/flexigrid.pack.js"></script>
+      <script type="text/javascript" src="js/kiosk.js"></script>
       <link rel="stylesheet" href="css/style.css" media="screen" />
       <link rel="stylesheet" href="css/print.css" media="print" />
       <link type="text/css" href="css/ui-darkness/jquery-ui-1.8.13.custom.css" rel="stylesheet" />
