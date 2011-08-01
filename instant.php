1d5be2900aa1847cb69f089a17ff1a3adcea2d6d
Andrew Reisner <andrew.reisner@gmail.com>
2011-08-01 09:17:11 -0700

    Started kiosk alternative

--- instant.php
+++ instant.php
@@ -12,17 +12,18 @@
     </head>
     <body>
         <div class="padded bodywrap">
-            <h1>Welcome to <?php echo(Settings::$name); ?>!</h1>
+            <h1>Welcome to <?php echo(Settings::$name); ?> (Instant Version)!</h1>
             <p>A service from the <?php echo(Settings::$organization); ?> to help you find a Valentine (or a new friend)</p>
             <div class="content">
                 <h2>Here's how it works:</h2>
-                    <p><b>1:</b> Enter your wartburg username.</p>
-		    <p><b>2:</b> Go to your wartburg email for a link to the quiz.</p>
+                    <p><b>1:</b> Enter Wartburg Username and take quiz.</p>
+		    <p><b>2:</b> Validate responses by clicking on validation link on your wartburg email.</p>
                     <p><b>3:</b> Order your top matches.</p>
                     <p><b>4:</b> Check your email.</p>
                 <h2>Ready to try?</h2>
                     <p>Tell us your email address:</p>
                     <form id="form" name="login">
+		        <input type="hidden" value="1" name="instant" />
                         <input name="email" id="email" class="validate[required]" type="email" placeholder="first.last" required="required" />@wartburg.edu
                         <input type="submit" id="submit" value="Submit"/>
                     </form>
