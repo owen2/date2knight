df0cfba596d66c96e2284f820ec742cd348b0f47
Andrew Reisner <andrew.reisner@gmail.com>
2011-08-10 09:28:43 -0700

    validated field in profile table

--- date2knight.sql
+++ date2knight.sql
@@ -33,6 +33,7 @@ CREATE TABLE `profile` (
   `seeks` tinyint(2) DEFAULT NULL,
   `paid` tinyint(1) DEFAULT NULL,
   `bio` text,
+  `validated` char(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -130,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-07-30 12:17:42
+-- Dump completed on 2011-08-10  9:28:26
