d379c2e9bae1958781df0770564bbae854762039
Andrew Reisner <andrew.reisner@gmail.com>
2011-08-23 09:40:19 -0700

    Finished new stats page

--- stats.php
+++ stats.php
@@ -19,6 +19,9 @@ require_once("functions.php");
 <?php
 $db = db_connect();
 $stmt = $db->stmt_init();
+$stmt1 = $db->stmt_init();
+$stmt2 = $db->stmt_init();
+$stmt3 = $db->stmt_init();
 $word[0] = 'person';
 $word[1] = 'people';
 
@@ -40,7 +43,56 @@ echo("There are ". $count[0] ." gays.<br>");
 $result = mysql_query("SELECT COUNT(*) FROM profile WHERE validated='true' AND seeks=2 AND gender=2");
 $count = mysql_fetch_row($result);
 echo("There are ". $count[0] ." lesbians.<br>");
+
+$sql = "SELECT id,text,lefttext,righttext FROM question WHERE enable=1";
+$sql1 = "SELECT count(response.id) from response inner join profile on (response.profile_id=profile.id) WHERE answer > 5 AND validated='true' AND response.question_id=?";
+$sql2 = "SELECT count(response.id) from response inner join profile on (response.profile_id=profile.id) WHERE answer=5 AND validated='true' AND response.question_id=?";
+$sql3 = "SELECT count(response.id) from response inner join profile on (response.profile_id=profile.id) WHERE answer<5 AND validated='true' AND response.question_id=?";
+
+echo '<br />';
+if ($stmt->prepare($sql) && $stmt1->prepare($sql1) && $stmt2->prepare($sql2) && $stmt3->prepare($sql3))
+{
+    $stmt1->bind_param('i',$id);
+    $stmt2->bind_param('i',$id);
+    $stmt3->bind_param('i',$id);    
+    $stmt->bind_result($id,$text,$left,$right);
+    $stmt1->bind_result($num1);
+    $stmt2->bind_result($num2);
+    $stmt3->bind_result($num3);
+    $stmt->execute();
+    $stmt->store_result();
+    echo "<center>";
+    echo "<table border='1'>";
+    while ($stmt->fetch())
+    {
+	echo "<tr><td align='center'><br />$left</td><td align='center'><b>$text</b><br />Don't care</td><td align='center'><br />$right</td></tr>";
+	$stmt1->execute();
+	$stmt1->store_result();
+	$stmt1->fetch();
+	echo "<tr><td align='center'>$num1</td>";
+	$stmt2->execute();
+	$stmt2->store_result();
+	$stmt2->fetch();
+	echo "<td align='center'>$num2</td>";
+	$stmt3->execute();
+	$stmt3->store_result();
+	$stmt3->fetch();
+	echo "<td align='center'>$num3</td></tr>";
+	$stmt1->free_result();
+	$stmt2->free_result();
+	$stmt3->free_result();
+    }
+    $stmt->close();
+    $stmt1->close();
+    $stmt2->close();
+    $stmt3->close();
+    $db->close();
+    echo "</table>";
+    echo "</center>";
+}
 ?>
+          <br />
+	  <a href="admin.php">[back]</a> <a href="killsession.php">[logout]</a>
         </div>
     </body>
 </html>
