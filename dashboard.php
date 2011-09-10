140146f98f5c8d4ba32adee77e85b5ed7636accb
Owen Johnson <owen@owenjohnson.info>
2011-09-09 20:50:26 -0700

    Profile updater dashlet prototype:

--- dashboard.php
+++ dashboard.php
@@ -77,7 +77,7 @@ $firstName = $profile['firstname'];
 $lastName = $profile['lastname'];
 $email = $profile['email'];
 $bio = $profile['bio'];
-$profileImage = "http://www.gravatar.com/avatar/".md5(strtolower(trim($email)))."?r=x&d=mm";
+$profileImage = "http://www.gravatar.com/avatar/".md5(strtolower(trim($email)))."?r=x&d=404";
 $valid = $profile['validated'];
 $paid = $profile['paid'];
 
@@ -114,11 +114,12 @@ function showQuiz()
 <head>
     <link rel="stylesheet" href="css/style.css" media="screen" />
     <link rel="stylesheet" href="css/print.css" media="print" />
+    <script type="text/javascript" src="js/jquery-1.6.3.min.js"></script>
 </head>
 <body>
     <div class="padded bodywrap">
         <div class="content">
-        <img src="<?php echo($profileImage);?>" class="stackright"/>
+        <a href="http://gravatar.com"><img src="<?php echo($profileImage);?>" class="stackright" alt="Set your picture!"/></a>
                 <h1><?php echo($firstName . " " . $lastName); ?></h1>
                 <?php echo($bio); ?>
             <br style="clear:both">
@@ -126,7 +127,15 @@ function showQuiz()
         <?php if ($valid == null)
         { 
         ?><h2 class="error">Identity Validation Pending</h2><p>Your profile won't be seen by others until you validate your email address. All you need to do is follow the link we sent you. This keeps people from pretending to be you.</p><?php
-        }
+        }?>
+        <h2>Your Profile</h2>
+        <form action="update-profile.php" method="post">
+            <p>My name is <?php echo($firstName." ".$lastName);?> and I am a <select>  <option value ="10">Man</option>  <option value ="01">Woman<option></select>, who is looking for a good time with <select>  <option value ="10">a Man</option>  <option value ="01">a Woman</option>  <option value ="11">a Man or a Woman</option>  <option value ="00">some new pals</option></select>.</p>
+            <p>People can send me mail at <?php echo($email);?> or my campus box <input type="number" class="short register-field" required="required" /> and call me at <input pattern="^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$" class="register-field"/>.</p>
+            <p>My friends would say <?php echo($firstName); ?> <input name="bio" required="required" class="long register-field">.</p>
+            <input type="submit"/>
+        </form>
+        <?php
         if (!hasTakenQuiz() and Settings::isPollOpen())
         {
             showQuiz();
